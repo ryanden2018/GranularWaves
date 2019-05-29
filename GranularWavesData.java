dd55fc69a5129a810e5eaf547f21187e96a8c13f
ryanden2018 <denlinger.ryan@gmail.com>
2019-05-29 11:02:32 -0500

    diagonal springs

--- GranularWavesData.java
+++ GranularWavesData.java
@@ -8,7 +8,8 @@ class GranularWavesData {
 
   GranularWavesData(double mass, int N, double dt, double width) {
     this.dt = dt;
-    int numSprings = 2*(N-2)*(N-3) + 4*(N-2);
+    //int numSprings = 2*(N-2)*(N-3) + 2*(N-2)*(N-3) + 4*(N-2) + (N-2);
+    int numSprings = (N-1)*(N-1)*4;
     springs = new Spring[numSprings];
     masses = new Mass[N*N];
     cursorX = (int) (width/2);
@@ -26,39 +27,87 @@ class GranularWavesData {
 
     int idx = 0; // spring index
 
-    // interior vertical springs
-    for( int i = 1; i < N-2; i++) {
-      for(int j=1; j < N-1; j++) {
-        springs[idx] = new Spring( masses[N*i+j], masses[N*(i+1)+j], dx, 1.0);
+    for( int i = 0; i < N-1; i++) {
+      for(int j = 0; j < N-1; j++) {
+        springs[idx] = new Spring( masses[N*i+j], masses[N*i+(j+1)], dx, 5.0);
+        idx++;
+        springs[idx] = new Spring( masses[N*i+j], masses[N*(i+1)+j], dx, 5.0);
+        idx++;
+        springs[idx] = new Spring( masses[N*i+j], masses[N*(i+1)+(j+1)], dx, 5.0);
+        idx++;
+        springs[idx] = new Spring( masses[N*i+(j+1)], masses[N*(i+1)+j], dx, 5.0);
         idx++;
       }
     }
 
-    // interior horizontal springs
-    for( int i = 1; i < N-1; i++) {
-      for(int j = 1; j < N-2; j++) {
-        springs[idx] = new Spring( masses[N*i+j], masses[N*i+(j+1)], dx, 1.0);
-        idx++;
+    // // interior vertical springs
+    // for( int i = 1; i < N-2; i++) {
+    //   for(int j=1; j < N-1; j++) {
+    //     springs[idx] = new Spring( masses[N*i+j], masses[N*(i+1)+j], dx, 5.0);
+    //     idx++;
+    //     springs[idx] = new Spring( masses[N*i+j], masses[N*(i+1)+(j+1)], dx, 5.0);
+    //     idx++;
+    //     springs[idx] = new Spring( masses[N*i+(j+1)], masses[N*(i+1)+j], dx, 5.0);
+    //     idx++;
+    //   }
+    // }
+
+    // // interior horizontal springs
+    // for( int i = 1; i < N-1; i++) {
+    //   for(int j = 1; j < N-2; j++) {
+    //     springs[idx] = new Spring( masses[N*i+j], masses[N*i+(j+1)], dx, 5.0);
+    //     idx++;
+    //   }
+    // }
+
+    // // left and right side springs
+    // for( int i = 1; i < N-1; i++) {
+    //   springs[idx] = new Spring( masses[N*i], masses[N*i+1], dx, 1.0);
+    //   idx++;
+    //   springs[idx] = new Spring( masses[N*i+(N-2)], masses[N*i+(N-1)], dx, 5.0);
+    //   idx++;
+    //   springs[idx] = new Spring( masses[N*i+(N-2)], masses[N*(i+1)+(N-1)],dx,5.0);
+    //   idx++;
+    // }
+
+    // // upper and lower side springs
+    // for( int j = 1; j < N-1; j++) {
+    //   springs[idx] = new Spring( masses[j], masses[N+j], dx, 1.0);
+    //   idx++;
+    //   springs[idx] = new Spring( masses[N*(N-2)+j], masses[N*(N-1)+j], dx, 5.0);
+    //   idx++;
+    // }
+  }
+
+  // update the internal data by one timestep following Newton's laws
+  void update() {
+    for( int i= 0; i < masses.length; i++) {
+      masses[i].posX += masses[i].vx*dt;
+      masses[i].posY += masses[i].vy*dt;
+    }
+
+    for( int i = 0; i < springs.length; i++) {
+      if( !springs[i].mass1.fixed ) {
+        springs[i].mass1.vx += ( springs[i].forceX() + nx(springs[i].mass1)*Math.pow(dist(springs[i].mass1)/(width*Math.sqrt(2)),2) - 0.005*springs[i].mass1.vx ) * dt;
+        springs[i].mass1.vy += ( springs[i].forceY() +  ny(springs[i].mass1)*Math.pow(dist(springs[i].mass1)/(width*Math.sqrt(2)),2) - 0.005*springs[i].mass1.vy ) * dt;
       }
-    }
 
-    // left and right side springs
-    for( int i = 1; i < N-1; i++) {
-      springs[idx] = new Spring( masses[N*i], masses[N*i+1], dx, 1.0);
-      idx++;
-      springs[idx] = new Spring( masses[N*i+(N-2)], masses[N*i+(N-1)], dx, 1.0);
-      idx++;
-    }
-
-    // upper and lower side springs
-    for( int j = 1; j < N-1; j++) {
-      springs[idx] = new Spring( masses[j], masses[N+j], dx, 1.0);
-      idx++;
-      springs[idx] = new Spring( masses[N*(N-2)+j], masses[N*(N-1)+j], dx, 1.0);
-      idx++;
+      if( !springs[i].mass2.fixed ) {
+        springs[i].mass2.vx += ( (-1)*springs[i].forceX() + nx(springs[i].mass2)*Math.pow(dist(springs[i].mass1)/(width*Math.sqrt(2)),2) - 0.005*springs[i].mass2.vx ) * dt;
+        springs[i].mass2.vy += ( (-1)*springs[i].forceY() + ny(springs[i].mass2)*Math.pow(dist(springs[i].mass1)/(width*Math.sqrt(2)),2) - 0.005*springs[i].mass2.vy ) * dt;
+      }
     }
   }
 
-  void update() {
+  double dist(Mass mass) {
+    return Math.sqrt(Math.pow(mass.posX-cursorX,2)+Math.pow(mass.posY-cursorY,2));
+  }
+
+  double nx(Mass mass) {
+    return (cursorX-mass.posX)/dist(mass);
+  }
+
+  double ny(Mass mass) {
+    return (cursorY-mass.posY)/dist(mass);
   }
 }
--- GranularWavesGraphics.java
+++ GranularWavesGraphics.java
@@ -7,8 +7,8 @@ class GranularWavesGraphics extends JComponent implements MouseMotionListener {
   int WIDTH = 650;
   int HEIGHT = WIDTH;
   double MASS = 1.0;
-  int N = 20;
-  double DT = 0.0001;
+  int N = 40;
+  double DT = 0.01;
   GranularWavesData gwd;
 
   GranularWavesGraphics() {
@@ -20,7 +20,9 @@ class GranularWavesGraphics extends JComponent implements MouseMotionListener {
   @Override
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
+    ((Graphics2D)g).setColor(Color.gray);
 
+    
     for(int i=0; i<(int)(1/DT); i++) {
       gwd.update();
     }
@@ -28,6 +30,13 @@ class GranularWavesGraphics extends JComponent implements MouseMotionListener {
     for(int i=0; i < gwd.masses.length; i++) {
       g.fillOval((int) (gwd.masses[i].posX-3), 
         (int) (gwd.masses[i].posY-3), 6, 6 );
+    }
+
+    for(int i = 0; i < gwd.springs.length; i++) {
+      g.drawLine(
+        (int) (gwd.springs[i].mass1.posX), (int) (gwd.springs[i].mass1.posY),
+        (int) (gwd.springs[i].mass2.posX), (int) (gwd.springs[i].mass2.posY)
+      );
     }
   }
 
